e658ecfca3e2687672e0ac1e576c2be001469e7f
jbhari <jbhari@hotmail.fr>
2022-02-04 15:59:03 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+// App.js
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from './MapView';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, marginBottom: 10, fontWeight: '600' }}>Behold, a map! ✨</Text>
+      <MapView
+        style={{
+          height: 300,
+          width: 300,
+          borderRadius: 20,
+          overflow: 'hidden',
+        }}
+      />
+      <StatusBar style="default" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ MapView.js
@@ -0,0 +1,28 @@
+// MapView.js
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+let MapView;
+
+if (Constants.appOwnership === 'expo') {
+  MapView = props => (
+    <View
+      style={[
+        {
+          backgroundColor: 'lightblue',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        props.style,
+      ]}>
+      <Text>🗺 (Mapbox not available)</Text>
+    </View>
+  );
+} else {
+  const Mapbox = require('@react-native-mapbox-gl/maps').default;
+  Mapbox.setAccessToken('pk.eyJ1IjoiamJoYXJpIiwiYSI6ImNreXlmeWZsYzBqczEydnFrZjZoeDJqMmEifQ.7Z9vGxLMr0cWskUyVAZXZQ');
+  MapView = Mapbox.MapView;
+}
+
+export default MapView;
